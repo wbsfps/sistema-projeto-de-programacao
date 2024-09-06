bb7154dd18b09ddf19de16697e3ee19d49e7a953
William Santana <williamandrade1058@gmail.com>
2024-09-05 22:49:17 -0300

    criando projeto

---
+++ src/test/java/br/com/GadoForte/projeto_GadoForte/ProjetoGadoForteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.GadoForte.projeto_GadoForte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoGadoForteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
